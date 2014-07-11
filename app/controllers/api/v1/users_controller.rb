702dee992351bffae269f164bfc754ad06019875
Etienne Depaulis <etienne.depaulis@gmail.com>
2014-07-11 14:22:44 +0200

    Added :show method on UsersController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,13 @@ class Api::V1::UsersController < Api::V1::BaseController
 	def index
 		respond_with User.all
 	end
+
+	def show
+		respond_with user
+	end
+
+	private
+		def user
+			User.find(params[:id])
+		end
 end
--- spec/api/v1/users_spec.rb
+++ spec/api/v1/users_spec.rb
@@ -3,13 +3,9 @@ require 'rails_helper'
 RSpec.describe '/api/v1/users', type: :api do
 
 	let(:url) { "/api/v1/users" }
+	let!(:user) { create :user, name: "Etienne" }
 
 	context 'index' do
-
-		before(:each) do
-		  create :user, name: "Etienne"
-		end
-
 		it "lists users" do
 			get "#{url}.json"
 
@@ -18,6 +14,18 @@ RSpec.describe '/api/v1/users', type: :api do
 			users = JSON.parse(last_response.body)
 
 			expect(users["users"].first["name"]).to eq "Etienne"
+		end
+	end
+
+	context 'show' do
+		it "shows a user" do
+			get "#{url}/#{user.id}.json"
+
+			expect(last_response.status).to eq 200
+
+			user = JSON.parse(last_response.body)
+
+			expect(user["user"]["name"]).to eq "Etienne"
 		end
 	end
 end
