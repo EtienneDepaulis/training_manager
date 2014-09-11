e1cb40d1437dde3a5de0103c7a5cde416218501d
Etienne Depaulis <etienne.depaulis@gmail.com>
2014-09-11 14:44:26 +0200

    Added dates on invitation serializer

--- app/serializers/invitation_serializer.rb
+++ app/serializers/invitation_serializer.rb
@@ -1,7 +1,7 @@
 class InvitationSerializer < ActiveModel::Serializer
 	embed :ids
 
-  attributes :id, :is_confirmed, :is_answered
+  attributes :id, :is_confirmed, :is_answered, :updated_at, :created_at
 
   has_one :training_session
   has_one :user, include: true
